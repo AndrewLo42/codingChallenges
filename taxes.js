75947254f582817072b887dcaa0f1319aabe1e6f
AndrewLo42 <andrewclo42@gmail.com>
2020-06-02 17:23:51 -0700

    completed stair case challenge

--- taxes.js
+++ taxes.js
@@ -1,4 +1,5 @@
- //It’s tax season! Your CPA has requested that you provide your tax amounts in cents this year. Rather than using pen and paper, wouldn’t it be faster for you to write a function that accepts a dollar amount and a tax percentage and returns the taxed amount in cents?
+ //It’s tax season! Your CPA has requested that you provide your tax amounts in cents this year. 
+ //Rather than using pen and paper, wouldn’t it be faster for you to write a function that accepts a dollar amount and a tax percentage and returns the taxed amount in cents?
 
 function taxes(dollars, percent) {
   let dollarAmount = dollars.split('');
