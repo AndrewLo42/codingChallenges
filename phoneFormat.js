664917389db87b63fe56c3467824e4a22e81401e
AndrewLo42 <andrewclo42@gmail.com>
2020-06-17 14:03:17 -0700

    tweeked to be more accurate answer

--- phoneFormat.js
+++ phoneFormat.js
@@ -2,7 +2,7 @@
 
 function phoneFormat(num) {
   let splitNum = (""+num).split('');
-  if (splitNum.length < 10) {
+  if (splitNum.length !== 10) {
     return ("Not valid 10 digit phone number");
   }
   return (`(${splitNum[0]}${splitNum[1]}${splitNum[2]}) ${splitNum[3]}${splitNum[4]}${splitNum[5]}-${splitNum[6]}${splitNum[7]}${splitNum[8]}${splitNum[9]}`)
