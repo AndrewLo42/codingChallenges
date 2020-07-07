e7eda98e0e03e7bb8b381a809f8e5102a95152db
AndrewLo42 <andrewclo42@gmail.com>
2020-07-07 15:13:12 -0700

    completed input addition challenge

---
+++ addInputs.js
@@ -0,0 +1,12 @@
+// Beginner programmer John Doe wants to make a program that adds and outputs each positive digit entered by the user(range is int).For instance, the result of 5528 is 20 and the result of 6714283 is 31.
+
+function addInput(input){
+  let splitInput = (""+input).split("");
+  let sum = 0;
+  for (let i = 0; i < splitInput.length; i++) {
+    sum += parseInt(splitInput[i]);
+  }
+  return sum;
+}
+
+console.log(addInput(5528))
