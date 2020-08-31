c5d129f11b428d4fc69e8f9e625a84ebc9684da0
AndrewLo42 <andrewclo42@gmail.com>
2020-08-30 22:45:58 -0700

    updated code

---
+++ evenorodd.js
@@ -0,0 +1,11 @@
+// Today's question, although it might seem easy to you, is a very common variation of a question asked often during coding interviews. Please write a function to check whether a number is even or not!
+
+function isEven(num) {
+  if(num % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isEven(2))
