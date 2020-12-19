0dc38e1646b53127a04227f9a87fd62ee1a44158
Andrew Lo <andrewclo42@gmail.com>
2020-12-18 19:54:47 -0800

    added no regex solution

--- stripComments.js
+++ stripComments.js
@@ -19,3 +19,14 @@ function solution(input, markers){
 }
 
 console.log(solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"]))
+
+//non regex
+function solutionNoRegex(input, markers) {
+  return input.split('\n').map(
+    line => markers.reduce(
+      (line, marker) => line.split(marker)[0].trim(), line
+    )
+  ).join('\n')
+}
+
+console.log(solutionNoRegex("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"]))
