9eaaee299a061a6ada762fc8bd4f93968d060fac
Andrew Lo <andrewclo42@gmail.com>
2024-07-19 10:37:30 -0700

    Update sqrtX.js

--- sqrtX.js
+++ sqrtX.js
@@ -3,6 +3,8 @@ Given a non-negative integer x, return the square root of x rounded down to the 
 
 You must not use any built-in exponent function or operator.
     For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python
+
+LC 69
 */
 
 var sqrt = function(x) {
